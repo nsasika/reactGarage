092c9cf1a6394eb0f8268b03b557467926c99042
Nalin Padmasiri <nalin.sasika@pagero.com>
2020-07-29 21:58:03 +0530

    Person component built with state dynamically

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi, I'm a React Application</h1>
-      </div>
-    );
-  }
+
+    state = {
+        persons:[
+            {name: "Sanath", age: "29"},
+            {name: "Pramod", age: "28"},
+            {name: "Madhawa", age: "32"}
+        ]
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <h1>Hi, I'm a React Application</h1>
+                <p>This is Really Working !</p>
+                <button>Switch Name</button>
+                <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Having two childeren !</Person>
+                <Person name={this.state.persons[2].name}age={this.state.persons[2].age}/>
+            </div>
+        );
+
+        /*return React.createElement('div',{className: 'App'},React.createElement('h1',null,'Hi, I\'m a React Application'))
+        * this is the reason why we use JSX in react.this createElement method may be much complex and hard to use
+        * */
+    }
 }
 
 export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const person = (props) => {
+    return (
+        <div>
+            <p>I'm a Person called {props.name} and I am {props.age} years Old</p>
+            <p>{props.children}</p>
+        </div>
+    )
+}
+
+/*here we passed props and children.we passed children where it may be a text,list or any object.see the inspect to see
+* the props.children in browser.
+* normaly use lowerCase names to name components(starting with lCase)
+* */
+
+export default person;
