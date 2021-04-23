147af38a34ce30d71532af8e85420baf14903e99
Nalin Padmasiri <nalin.sasika@pagero.com>
2021-04-23 13:02:06 +0530

    VirtualDOM vs RealDOM practical files added

--- src/index.js
+++ src/index.js
@@ -2,7 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App country="Sri lanka"/>, document.getElementById('root'));
-registerServiceWorker();
+function updateTime() {
+    const VirtualDOM = (
+        <div>
+            <h1>Example of Virtual DOM Rendering</h1>
+            <h1>Hello World!</h1>
+            <h1>Now the time is</h1>
+            <p>{new Date().toLocaleTimeString()}</p>
+        </div>
+    )
+    // renders component VirtualDOM to the index.html's( inside public folder) div of id root
+    ReactDOM.render(VirtualDOM,
+        document.getElementById('root')
+    );
+}
+
+// calls the function updateTime in every 2 seconds (2000 milliseconds):
+setInterval(updateTime, 2000);
+
+
+
+//ReactDOM.render(<App country="Sri lanka"/>, document.getElementById('root'));
+
